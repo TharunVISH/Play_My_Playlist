05f8919f78b376ab2a3d3b9a86dc53f91c10e382
Shriya003 <84763562+Shriya003@users.noreply.github.com>
2021-09-19 15:07:22 +0530

    Add files via upload

---
+++ src/Createpage/Createpage.js
@@ -0,0 +1,57 @@
+import './Createpage.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopy } from '@fortawesome/fontawesome-free-solid'
+import MusicImage from '../ImagesUsed/Music3.jpg';
+
+
+function Createpage() {
+    return(
+        <div className= "CreatepageHead">
+            <h1 className="head1">Create Room <button>Room ID:THYE  <FontAwesomeIcon icon={faCopy} /></button></h1>
+            
+  <Form>
+  <Row>
+    <Col>
+    <Form.Group className="Block-1" >
+    <Form.Label><h3>Room Name:</h3></Form.Label>
+      <Form.Control  type="Text"  />
+      </Form.Group>
+    </Col>
+    <Col>
+    <Form.Group className="Block-2">
+    <Form.Label><h3>Number of Participants:</h3></Form.Label>
+      <Form.Control type="number" />
+      </Form.Group>
+    </Col>
+  </Row>
+</Form>
+
+<Form>
+  <Row>
+    <Col>
+    <Form.Group className="Block-3" >
+    <Form.Label><h3>Passcode:</h3></Form.Label>
+    <Button><FontAwesomeIcon icon={faCopy} /></Button>
+      <Form.Control  type="password"/>
+      </Form.Group>
+    </Col>
+    <Col>
+    <Form.Group className="Block-4">
+    <Form.Label><h3>Game Rule:</h3></Form.Label>
+      <Form.Control type="text"/>
+      </Form.Group>
+    </Col>
+  </Row>
+</Form>
+<button className="gen-code">Generate code</button>
+<div className="bottom"><button className="btn-bottom">Create Room </button></div>
+
+        </div>
+    )
+}
+export default Createpage;
