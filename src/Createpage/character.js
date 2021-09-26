841a741a05def41efbaec6bd3fab55a9231a3739
Shriya003 <sriyasri003@gmail.com>
2021-09-27 00:04:37 +0530

    added the functionality

---
+++ src/Createpage/character.js
@@ -0,0 +1,4 @@
+export const numbers= '0123456789'
+export const uppercaselettters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+export const lowercaseletters='abcdefghijklmnopqrstuvwxyz'
+export const specialcharacter='!@#$%^&*()~`,.<>?/'
